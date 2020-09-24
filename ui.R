69cc0467eb1188a1a9b804ffb3cfd95110c2b770
oz <oz@vps779465.ovh.net>
2020-09-24 11:13:03 +0200

    label column check added. backwards.compatible code added

--- ui.R
+++ ui.R
@@ -6,14 +6,24 @@
 #
 #    http://shiny.rstudio.com/
 #
+# backwards compatibility
+# windows R-3.4.1 (current dwr-next env.) 
+# using latest 3.4.x windows-binaries (R3.4.4) so you don't have to compile
+#setRepositories(addURLs = c(MRAN_R344="https://cran.microsoft.com/snapshot/2018-04-01/"),ind=0) #R3.4.4
+#install.packages("htmlwidgets") #new versions ask for shiny 1.1 and throws error
+#install.packages("DT") #new versions ask for shiny 1.1 and throws error
+#install.packages("shiny") #this installs shiny 1.0.5
 
-
+library(htmlwidgets )
+library(DT)
 library(shiny)
+
 library(leaflet)
 library(RColorBrewer)
 library(scales)
 library(lattice)
 library(dplyr)
+
 
 # Define UI for application that draws a histogram
 ui <- shinyUI(fluidPage(
